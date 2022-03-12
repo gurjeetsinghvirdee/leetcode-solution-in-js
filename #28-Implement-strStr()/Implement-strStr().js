50c474c8c3b9cf17c03df4c78fa3f446f48d48be
gurjeetsinghvirdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-03-12 23:05:58 +0530

    Solution #28 Added

---
+++ #28-Implement-strStr()/Implement-strStr().js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function (haystack, needle) {
+    
+    if (haystack == null || needle == null) {
+        return -1;
+    }
+    
+    if (haystack === needle) {
+        return 0;
+    }
+    
+    const needleLength = needle.length;
+
+    for (let i = 0; i < haystack.length - needleLength + 1; i++) {
+
+        if (haystack.substring(i, i + needleLength) === needle) {
+
+            return i;
+        }
+    }
+    return -1;
+};
