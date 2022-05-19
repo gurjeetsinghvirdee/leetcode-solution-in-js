35a0768331397baed86f9c8de47f1d18046b11c0
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-05-19 20:49:59 +0530

    Isomorphic files added

--- #205-Isomorphic-Strings/Isomorphic Strings.js
+++ #205-Isomorphic-Strings/Isomorphic Strings.js
@@ -1,33 +1,24 @@
-
-
-```javascript
 /**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
 
-const isIsomorphic = function(s, t) {
-
+ var isIsomorphic = function(s, t) {
     if (s.length !== t.length) return false;
-    let sDict = new Map();
-    let tDict = new Map();
-    let (let i = 0; i < s.length; i++ ) {
-        let sc = s[i];
-        let tc = t[i];
-
+    var sDict = new Map();
+    var tDict = new Map();
+    for (var i = 0; i < s.length; i++ ) {
+        var sc = s[i];
+        var tc = t[i];
         if (!sDict.has(sc) && !tDict.has(tc)) {
             sDict.set(sc,tc);
             tDict.set(tc,sc);    
         }
- 
+
         if (sDict.get(sc) !== tc || tDict.get(tc) !== sc) {
             return false;
         }
-
     }
-
     return true;
-    
 };
-```
